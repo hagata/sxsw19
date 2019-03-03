b322b509c5b95eda1ebdd4f933d9f965fa2dbc99
Genki Hagata <ghagata@hugeinc.com>
2019-03-03 08:10:01 -0800

    chapter 1 complete, ch2 base

--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -1,19 +1,177 @@
 <template>
-    <div>
-        <!-- Audio Player -->
-        <!-- Time controls -->
-            <!-- Timestamps, current time and total time -->
-            <!-- seekbar & seekbar fill element -->
-        <!-- Play button -->
+    <div class="controls">
+        <audio
+        ref="podcastAudio"
+        preload="auto"
+        v-on:loadedmetadata="audioMetadataHandler()"
+        v-on:timeupdate="currentTimeHandler"
+        :src="audio"></audio>
+
+        <div class="controls__time">
+            <div class="controls__timestamps">
+                <span ref="currentTimeIndicator" class="controls__timestamp">{{currentTime}}</span>
+                <span ref="totalTimeIndicator" class="controls__timestamp">{{duration}}</span>
+            </div>
+            <div
+            v-on:click="seekHandler($event)"
+            ref="seekbar"
+            class="controls__seekbar">
+                <span ref="seekbarFill" class="controls__seekbar-fill"></span>
+            </div>
+        </div>
+
+        <button
+        ref="playToggleButton"
+        class="controls__toggle-button"
+        @click="playHandler($event)"
+        >
+        <!-- toggle button state from play/pause -->
+        <span v-if="playing">PAUSE</span>
+        <span v-if="!playing">PLAY</span>
+        </button>
     </div>
+
 </template>
 
 <script>
     export default {
         name: 'Controls',
+        data() {
+            return {
+                currentTime: '0:00',
+                duration: '',
+                playing: false,
+            }
+        },
+        props: [
+            'audio'
+        ],
+        methods: {
+            /**
+             * When The play/pause button is pushed, play or pause the audio
+             */
+            playHandler() {
+                if (this.$refs.podcastAudio.paused) {
+                    this.$refs.podcastAudio.play();
+                    this.currentTimeHandler();
+                } else {
+                    this.$refs.podcastAudio.pause();
+                }
+                this.playing = !this.playing
+            },
+            /**
+             * When the podcast is playing, we should be constantly updating
+             * the current time
+             */
+            currentTimeHandler() {
+                // console.log('Current Time', this.$refs.podcastAudio.currentTime)
+                this.currentTime =
+                    this.secondsToHHMMSS(this.$refs.podcastAudio.currentTime)
+
+                // When the audio player time changes, update the seekbar fill
+                this.seekbarFillHandler();
+            },
+            /**
+             * Meta data handler
+             * Get's audio files duration when it's available and sets
+             * the duration prop
+             */
+            audioMetadataHandler() {
+                const seconds = this.$refs.podcastAudio.duration;
+                this.duration = this.secondsToHHMMSS(seconds);
+            },
+            /**
+             * Timebar handler
+             * takes the current time, set's a fill indicator on the seekbar
+             */
+            seekbarFillHandler() {
+                // get the percentage of the audio player from current time
+                const percentage =
+                    this.$refs.podcastAudio.currentTime /
+                    this.$refs.podcastAudio.duration;
+
+                this.$refs.seekbarFill.style.transform = `translateX(${-100 + percentage * 100}%)`
+            },
+            /**
+             * When the seekbar is clicked, get the position of the click,
+             * updates the audio position to the clicked position (percentage)
+             * calls fill update
+             */
+            seekHandler(event) {
+                const click = event.clientX;
+                // get seekbar position and size
+                const seekbarWidth = this.$refs.seekbar.getBoundingClientRect().width;
+                const seekbarOffset = this.$refs.seekbar.getBoundingClientRect().left;
+                const percentage = (click - seekbarOffset) / (seekbarWidth);
+
+                const seekToTimeInSeconds = this.$refs.podcastAudio.duration * percentage;
+                this.$refs.podcastAudio.currentTime = seekToTimeInSeconds;
+
+                this.seekbarFillHandler();
+            },
+            /**
+             * duration to time utillity
+             * takes time in s., converts it to hh mm ss
+             */
+            secondsToHHMMSS(seconds) {
+                let duration = new Date(null);
+                duration.setSeconds(seconds);
+                return duration.toISOString() // full date
+                    .substr(11, 8) // just the time
+                    .replace(/^0(?:0:0?)?/, ""); // removes 00's before :
+            }
+        }
     }
 </script>
 
 <style lang='scss' scoped>
+    .controls {
+        align-items: center;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        outline: 1px dashed tomato;
+        width: 80%;
 
+        &__time {
+            align-items: center;
+            display: flex;
+            flex-direction: column;
+        }
+        &__seekbar {
+            background: #aaaaaa;
+            border-radius: 5px;
+            height: 16px;
+            overflow: hidden;
+            position: relative;
+            width: 60vw;
+        }
+
+        &__seekbar-fill {
+            background: var(--dark-blue);
+            height: 100%;
+            left: 0;
+            position: absolute;
+            top: 0;
+            transform: translateX(-100%); //sets start position to 0
+            width: 100%;
+        }
+
+        &__timestamps {
+            display: flex;
+            justify-content: space-between;
+            width:100%;
+        }
+
+        &__toggle-button {
+            background: var(--dark-blue);
+            border: none;
+            border-radius: 50%;
+            color: white;
+            display: block;
+            height: 44px;
+            width: 44px;
+        }
+
+    }
 </style>
--- src/components/PodcastPlayer.vue
+++ src/components/PodcastPlayer.vue
@@ -2,30 +2,56 @@
 <template>
   <div class="podcast-player">
     <!-- outputs the podcast data to the page -->
+    <!-- <pre>{{currentPodcast}}</pre> -->
     <!-- Build out the player UI -->
     <!-- Podcast cover art -->
+    <img :src="currentPodcast.artwork" :alt="`artwork for ${currentPodcast.podcast}`">
 
     <!-- Podcast Title & Episode name -->
+    <span class="podcast-player__title">
+      {{currentPodcast.podcast}}
+    </span>
+    <h1 class="podcast-player__episode">
+      {{currentPodcast.episodeTitle}}
+    </h1>
     <!-- Controls Component
       make a new component in components/Controls.vue
       it will take in the currentEpisode.audioSource as a prop
+
     -->
+    <controls
+      :audio="currentPodcast.audioSource"
+     />
+
+    <p class="podcast-player__description">
+      {{currentPodcast.description}}
+    </p>
     <!-- Podcast Description -->
   </div>
 </template>
 
 <script>
-// import podcast data from data/
+import podcasts from '../data/podcast-data.js';
+import Controls from './Controls.vue';
+
 export default {
   name: 'PodcastPlayer',
   data() {
     return {
       // Return a podcast as currentPodcast
     };
+  },
+  components: {
+    Controls
   }
 };
 </script>
 
 <style lang='scss' scoped>
+.podcast-player {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+}
 
 </style>
